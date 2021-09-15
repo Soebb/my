a9951ce7db3136ea96bc41e1dbb3771812052c09
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-15 14:58:29 +0430

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -3,105 +3,90 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import asyncio
+import asyncio, io
 from pyrogram import filters
 from bot import autocaption
 from config import Config
 from database.database import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
+from music_tag import load_file
+from PIL import Image
 
+@autocaption.on_message(filters.private & filters.audio)
+async def tag(bot, m):
+    fname = m.audio.file_name
+    m = await bot.get_messages(m.chat.id, m.message_id)
+    file = await m.download(file_name="temp/file.mp3")
+    await m.delete()
+    music = load_file("temp/file.mp3")
+    t = f"{music['title']}"
+    a = f"{music['artist']}"
+    al = f"{music['album']}"
+    g = f"{music['genre']}"
+    c = f"{music['comment']}"
+    l = f"{music['lyrics']}"
+    ar = music['artwork']
+    image_data = ar.value.data
+    img = Image.open(io.BytesIO(image_data))
+    img.save("artwork.jpg")
+  
+    if fname.split(' ')[0].__contains__("@") or fname.split(' ')[0].__contains__(".me/"):
+        fname = fname.split(f"{fname.split(' ')[0]}")[+1]
+    elif (fname.__contains__("@") or fname.__contains__(".me/")) and ((not fname.split(' ')[0].__contains__("@")) and (not fname.split(' ')[0].__contains__(".me/"))):
+        fname = fname.split(f"{fname.rsplit(' ', 1)[1]}")[0]
 
-@autocaption.on_message(~filters.edited, group=-1)
-async def editing(bot, message):
-    if (message.chat.type == "private"):
-        if ("/set_cap" in message.text) and ((len(message.text.split(' ')) == 2) or (len(message.text.split(' ')) == 1)):
-            await message.reply_text("🖊️ 𝐒𝐄𝐓 𝐂𝐀𝐏𝐓𝐈𝐎𝐍 \n\nUse this command to set custom caption for any of your channels.\n\n👉 `/set_cap -1001448973320 My Caption`", quote = True)
-        elif ("/set_cap" in message.text) and (len(message.text.split(' ')) != 2) and (len(message.text.split(' ')) != 1):
-            caption = message.text.markdown.split(' ', 2)[2]
-            channel = message.text.split(' ', 2)[1].replace("-100", "")
-            try:
-                cap = await get_caption(int(channel))
-                c = cap.caption
-            except:
-                await update_caption(channel, caption)
-                return await message.reply_text(f"**--Your Caption--:**\n\n{caption}", quote=True)
-            await message.reply_text("⚠️\n\nA caption already seted for this channel, you should first use /rmv_cap command to remove the current caption and then try seting new.", quote=True)
-           
-        if ("/set_btn" in message.text) and ((len(message.text.split(' ')) == 2) or (len(message.text.split(' ')) == 1)):
-            await message.reply_text("🖊️ 𝐒𝐄𝐓 BUTTON \n\nUse this command to set button for any of your channels.\nSend a Button name and URL(separated by ' | ').\n\n👉 `/set_btn -1001448973320 Channel | https://t.me/channel`", quote = True)
-        elif ("/set_btn" in message.text) and (len(message.text.split(' ')) != 2) and (len(message.text.split(' ')) != 1):
-            button = message.text.split(' ', 2)[2]
-            channel = message.text.split(' ', 2)[1].replace("-100", "").replace("1", "")
-            try:
-                btn = await get_button(int(channel))
-                b = btn.button
-            except:
-                await update_button(channel, button)
-                return await message.reply_text(f"**--Your Button--:**\n\n{button}", quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button.split(' | ')[0], url=f"{button.rsplit(' ', 1)[1]}")]]))
-            await message.reply_text("⚠️\n\nA button already seted for this channel, you should first use /rmv_btn command to remove the current button and then try seting new.", quote=True)
-           
-        if (message.text == "/rmv_cap"):
-            await message.reply_text("Use this command to remove the current caption of any of your channels.\n\n👉 `/rmv_cap -1001448973320`", quote = True)
-        elif ("/rmv_cap" in message.text) and (len(message.text.split(' ')) != 1):
-            channel = message.text.split(' ', 1)[1].replace("-100", "")
-            try:
-                cap = await get_caption(int(channel))
-                c = cap.caption
-            except:
-                return await message.reply_text("Caption not setted yet!", quote=True)     
-            await del_caption(channel)
-            await message.reply_text("✅The Caption Removed Successfully.", quote=True)
+    if a.split(' ')[0].__contains__("@") or a.split(' ')[0].__contains__(".me/"):
+        a = a.split(f"{a.split(' ')[0]}")[+1]
+    elif (a.__contains__("@") or a.__contains__(".me/")) and ((not a.split(' ')[0].__contains__("@")) and (not a.split(' ')[0].__contains__(".me/"))):
+        a = a.split(f"{a.rsplit(' ', 1)[1]}")[0]
+     
+    if al.split(' ')[0].__contains__("@") or al.split(' ')[0].__contains__(".me/"):
+        al = al.split(f"{al.split(' ')[0]}")[+1]
+    elif (al.__contains__("@") or al.__contains__(".me/")) and ((not al.split(' ')[0].__contains__("@")) and (not al.split(' ')[0].__contains__(".me/"))):
+        al = al.split(f"{al.rsplit(' ', 1)[1]}")[0]
 
-        if (message.text == "/rmv_btn"):
-            await message.reply_text("Use this command to remove the current button of any of your channels.\n\n👉 `/rmv_btn -1001448973320`", quote = True)
-        elif ("/rmv_btn" in message.text) and (len(message.text.split(' ')) != 1):
-            channel = message.text.split(' ', 1)[1].replace("-100", "").replace("1", "")
-            try:
-                btn = await get_button(int(channel))
-                b = btn.button
-            except:
-                return await message.reply_text("Button not setted yet!", quote=True)     
-            await del_button(channel)
-            await message.reply_text("✅The Button Removed Successfully.", quote=True)
+    if c.split(' ')[0].__contains__("@") or c.split(' ')[0].__contains__(".me/"):
+        c = c.split(f"{c.split(' ')[0]}")[+1]
+    elif (c.__contains__("@") or c.__contains__(".me/")) and ((not c.split(' ')[0].__contains__("@")) and (not c.split(' ')[0].__contains__(".me/"))):
+        c = c.split(f"{c.rsplit(' ', 1)[1]}")[0]
 
-    if (message.chat.type == "channel") and (message.video or message.document or message.audio):
-        m = message.video or message.document or message.audio
-        try:
-            channel = str(message.chat.id).replace('-100', '').replace('1', '')
-            btn = await get_button(int(channel))
-            button = btn.button
-        except:
-            button = None
-            pass
-        try:
-            channel = str(message.chat.id).replace('-100', '')
-            cap = await get_caption(int(channel))
-            if message.audio:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{artist}", f"{m.performer}").replace("{title}", f"{m.title}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
-            elif message.video:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{width}", f"{str(m.width)}").replace("{height}", f"{str(m.height)}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
-            elif message.document:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
-        except:
-            caption = None
-            pass
-       
-        if button is not None:
-            Url = button.rsplit(' ', 1)[1]
-            Name = button.split(' | ')[0]
-            if caption is not None:
-                try:
-                    await bot.edit_message_caption(chat_id = message.chat.id, message_id = message.message_id, caption = caption, parse_mode = "markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(Name, url=f"{Url}")]]))
-                except Exception as e:
-                    print(e)
-            elif caption is None:
-                try:
-                    await bot.edit_message_caption(chat_id = message.chat.id, message_id = message.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(Name, url=f"{Url}")]]))
-                except Exception as e:
-                    print(e)
-        elif (button is None) and (caption is not None):
-            try:
-                await bot.edit_message_caption(chat_id = message.chat.id, message_id = message.message_id, caption = caption, parse_mode = "markdown")
-            except Exception as e:
-                print(e)
+    if l.split(' ')[0].__contains__("@") or l.split(' ')[0].__contains__(".me/"):
+        l = l.split(f"{l.split(' ')[0]}")[+1]
+    elif (l.__contains__("@") or l.__contains__(".me/")) and ((not l.split(' ')[0].__contains__("@")) and (not l.split(' ')[0].__contains__(".me/"))):
+        l = l.split(f"{l.rsplit(' ', 1)[1]}")[0]
+
+    if t.split(' ')[0].__contains__("@") or t.split(' ')[0].__contains__(".me/"):
+        t = t.split(f"{t.split(' ')[0]}")[+1]
+    elif (t.__contains__("@") or t.__contains__(".me/")) and ((not t.split(' ')[0].__contains__("@")) and (not t.split(' ')[0].__contains__(".me/"))):
+        t = t.split(f"{t.rsplit(' ', 1)[1]}")[0]
+
+    if g.split(' ')[0].__contains__("@") or g.split(' ')[0].__contains__(".me/"):
+        g = g.split(f"{g.split(' ')[0]}")[+1]
+    elif (g.__contains__("@") or g.__contains__(".me/")) and ((not g.split(' ')[0].__contains__("@")) and (not g.split(' ')[0].__contains__(".me/"))):
+        g = g.split(f"{g.rsplit(' ', 1)[1]}")[0]
+
+    music.remove_tag('comment')
+    music.remove_tag('artist')
+    music.remove_tag('lyrics')
+    music.remove_tag('title')
+    music.remove_tag('album')
+    music.remove_tag('genre')
+    music['artist'] = a + Config.custom_tag
+    music['title'] = t + Config.custom_tag
+    music['album'] = al + Config.custom_tag
+    music['genre'] = g + Config.custom_tag
+    music['comment'] = c + Config.custom_tag
+    music['lyrics'] = l + Config.custom_tag
+    music.save()
+
+    try:
+        await bot.send_audio(
+            chat_id=m.chat.id,
+            file_name=fname + ".mp3",
+            caption=caption,
+            thumb=open('artwork.jpg', 'rb'),
+            audio="temp/file.mp3"
+        )
+    except Exception as e:
+        print(e)
